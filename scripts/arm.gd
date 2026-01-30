20c2d27951b9ed6d7e1c4babbacd1a6da125e122
dch777 <d.chanana777@gmail.com>
2026-01-30 17:07:58 -0500

    ball

--- scripts/arm.gd
+++ scripts/arm.gd
@@ -1,10 +1,25 @@
 class_name Arm extends RigidBody2D
 
-# func _integrate_forces(state: PhysicsDirectBodyState2D):
-# 	var facing = Vector2.from_angle(rotation)
-# 	state.apply_torque(1000 * facing.angle_to(get_local_mouse_position()))
+@export var kp: float = 1000.0
+@export var ki: float = 100.0
+@export var kd: float = 500.0
 
-func _physics_process(delta):
-	# var facing = Vector2.from_angle(rotation)
-	# print(get_local_mouse_position().angle_to(facing))
-	look_at(get_global_mouse_position())
+var _prev_error: float = 0.0
+var _integral: float = 0.0
+var _int_max = 200
+
+func _integrate_forces(state: PhysicsDirectBodyState2D):
+	var error = wrapf(get_global_mouse_position().angle_to_point(position) - global_rotation, -PI, PI)
+	var delta = state.get_step()
+
+	var P = kp * error
+
+	_integral = wrapf(_integral + (error * delta), -256, 256)
+	var I = ki * _integral
+
+	var derivative = (error - _prev_error) / delta
+	var D = kd * derivative
+
+	state.apply_torque(P + I + D)
+
+	_prev_error = error
